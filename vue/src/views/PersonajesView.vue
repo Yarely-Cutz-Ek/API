a2d4475788b361b9f690a96a0185bfabcedd1719
Yarely-Cutz-Ek <yarely.19070025@itsmotul.edu.mx>
2023-02-28 13:28:46 -0600

    Base

---
+++ vue/src/views/PersonajesView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Personajes from "../components/Personajes.vue";
+</script>
+
+<template>
+  <div>
+    <h1>Personajes del programa</h1>
+
+    <Personajes />
+  </div>
+</template>
+
+<style>
+</style>
